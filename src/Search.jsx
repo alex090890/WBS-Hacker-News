bd739689b6c30a64543aef1e143bb7a9a94917c4
Oleksandr Lazurenko <alexprofteach@gmail.com>
2024-02-10 12:35:16 +0000

    Worked on styling

--- src/Search.jsx
+++ src/Search.jsx
@@ -4,8 +4,10 @@ import axios from "axios";
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchNews = async (query) => {
+    setLoading(true);
     try {
       const response = await axios.get(
         `https://hn.algolia.com/api/v1/search?query=${query}&tags=story`,
@@ -13,6 +15,8 @@ const Search = () => {
       setNews(response.data.hits);
     } catch (error) {
       console.error("Error fetching news:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -33,15 +37,21 @@ const Search = () => {
         <button type="submit">Search</button>
       </form>
       <div className="newsList">
-        {news.map((article) => (
-          <div key={article.objectID} className="newsContainer">
-            <h2 className="newsTitle">{article.title}</h2>
-            <span className="handIcon">&#128073;</span>
-            <a href={article.url} className="newsUrl">
-              Click Here
-            </a>
-          </div>
-        ))}
+        {loading ? (
+          <div className="process">Loading...</div>
+        ) : news.length > 0 ? (
+          news.map((article) => (
+            <div key={article.objectID} className="newsContainer">
+              <h2 className="newsTitle">{article.title}</h2>
+              <span className="handIcon">&#128073;</span>
+              <a href={article.url} className="newsUrl">
+                Click Here
+              </a>
+            </div>
+          ))
+        ) : (
+          news.length === 0 && <div className="process">Type something</div>
+        )}
       </div>
     </div>
   );
