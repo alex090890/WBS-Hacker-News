5da474d88b1a85a90cc80ace461842197754b645
Oleksandr Lazurenko <alexprofteach@gmail.com>
2024-02-10 10:46:11 +0000

    Added some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Search from "../Search";
+import Search from "./Search";
 import "./styles.css";
 
 export default function App() {
--- src/Search.jsx
+++ src/Search.jsx
@@ -31,12 +31,17 @@ const Search = () => {
         />
         <button type="submit">Search</button>
       </form>
-      {news.map((article) => (
-        <div key={article.objectID} className="newsContainer">
-          <h2 className="newsTitle">{article.title}</h2>
-          <p className="newsUrl">{article.url}</p>
-        </div>
-      ))}
+      <div className="newsList">
+        {news.map((article) => (
+          <div key={article.objectID} className="newsContainer">
+            <h2 className="newsTitle">{article.title}</h2>
+            <span className="handIcon">&#128073;</span>
+            <a href={article.url} className="newsUrl">
+              Click Here
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
